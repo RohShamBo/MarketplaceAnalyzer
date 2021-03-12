34a11e4563bd80fe8d40ad71beebd3367830b768
RohShamBo <80442111+RohShamBo@users.noreply.github.com>
2021-03-12 04:01:25 +0000

    Update background.js

--- background.js
+++ background.js
@@ -1,15 +1,8 @@
 function listener(tabId, changeInfo, tab) {
-    console.log('I ran');
     if (tab.url.search("://www.nbatopshot.com/listings/p2p/") > -1 && changeInfo.status == 'complete') {
         chrome.tabs.executeScript(tab.id, {
-            file: 'foreground.js'
+            file: 'foreground.js'  
         });
     }
 }
 chrome.tabs.onUpdated.addListener(listener);
-//chrome.browserAction.onClicked.addListener(listener);
-
-//for popup in json
-//"browser_action": {
-//    "default_popup": "popup.html"
-//},
